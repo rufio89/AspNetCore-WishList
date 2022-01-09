994534e944b2e85e6c43c72a186caac2e2dd2c42
Ryan Krienitz <r.krienitz@gmail.com>
2022-01-08 21:56:16 -0500

    A

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -17,7 +17,28 @@ namespace WishList.Controllers
 
         public IActionResult Index()
         {
-            return View("Index");
+            var model = _context.Items.ToList();
+
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Create() {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Models.Item item) {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id) {
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
